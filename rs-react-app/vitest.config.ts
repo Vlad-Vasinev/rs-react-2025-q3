edd20963e0c11c151fbce821a61c8d1a8be2c1ce
Vlad-Vasinev <vda5petr@rambler.ru>
2025-07-19 17:41:00 +0300

    setted up coverage

--- rs-react-app/vitest.config.ts
+++ rs-react-app/vitest.config.ts
@@ -4,6 +4,24 @@ export default defineConfig ({
   test: {
     globals: true, 
     environment: "jsdom", 
-    setupFiles: './test/setup'
-  }
+    setupFiles: './test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts'
+      ],
+      thresholds: { 
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      }
+    },
+  },
 })
