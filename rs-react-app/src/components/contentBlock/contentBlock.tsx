b4e2916b25f0d7617151a4decbe395223dd0b4a0
Vlad-Vasinev <vda5petr@rambler.ru>
2025-07-25 11:29:16 +0300

    fixed functional components

--- rs-react-app/src/components/contentBlock/contentBlock.tsx
+++ rs-react-app/src/components/contentBlock/contentBlock.tsx
@@ -1,12 +1,8 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import loaderIcon from '../../assets/general/loadingIcon.svg';
 import ErrorBtn from '../errorBtn/errorBtn';
 import HandleForm from '../handleForm/handleForm';
-
-interface ContentBlockProps {
-  title?: string,
-}
 
 interface BerryFirmness {
   name: string, 
@@ -58,141 +54,147 @@ interface ContentBlockState {
   searchResult: string | null,
 }
 
-class ContentBlock extends Component<ContentBlockProps, ContentBlockState> {
+const ContentBlock = () => {
 
-  constructor (props: ContentBlockProps) {
-    super(props);
-    this.state = {
-      data: null,
-      loading: true, 
-      errorMessage: false,
-      fetchResult: null, 
-      searchResult: '',
-    }
+  const [contentState, setContentState] = useState<ContentBlockState>({
+    data: null,
+    loading: true, 
+    errorMessage: false,
+    fetchResult: null, 
+    searchResult: '',
+  })
+  
+  function ErrorClick () {
+    setContentState(prev => ({
+      ...prev, 
+      errorMessage: true
+    }))
   }
 
-  ErrorClick = () => {
-    this.setState({errorMessage: true})
-  }
-
-  onClick = () => {
+  function onClick () {
 
     const storedData = localStorage.getItem('resultRequest');
     let parsedData: BerryDate;
 
     if (storedData) {
       parsedData = JSON.parse(storedData)
-      this.setState({fetchResult: parsedData})
+      setContentState(prev => ({
+        ...prev, 
+        fetchResult: parsedData
+      }))
     }
 
-    this.setState({searchResult: localStorage.getItem('inputValue')})
+    setContentState(prev => ({
+      ...prev, 
+      searchResult: localStorage.getItem('inputValue')
+    }))
   }
 
-  componentDidMount() {
 
+  useEffect(() => {
     setTimeout(() => {
       fetch('https://pokeapi.co/api/v2/berry')
         .then(response => {
           return response.json()
         })
         .then(result => {
-          this.setState({data: result.results, loading: false})
+
+          setContentState(prev => ({
+            ...prev, 
+            data: result.results, 
+            loading: false
+          }))
         })
     }, 3000)
+  }, [])
+
+  if(contentState.errorMessage) {
+    throw new Error("I am an artificial error!");
   }
-  
-  render() {
 
-    if(this.state.errorMessage) {
-      throw new Error("I am an artificial error!");
-    }
-
-    const { data, loading } = this.state
-
-    if(loading) {
-      return (
-        <section className='contentBlock' data-testid="content-block">
-          <div className='contentBlock__top'>
-            <HandleForm ></HandleForm>
-          </div>
-          <div className='contentBlock__middle'>
-            <div className='loadingIcon'>
-              <img data-testid="loader-icon" src={loaderIcon} alt='loading icon'></img>
-            </div>
-          </div>
-        </section>
-      )
-    }
-
-    if(this.state.searchResult) {
-      return (
-        <section className='contentBlock' data-testid="content-block">
-          <div className='contentBlock__top'>
-            <HandleForm onClick={this.onClick}></HandleForm>
-          </div>
-          <div className='contentBlock__middle'>
-            <ul className='listApi'>
-              <li className='listApi__el-info'>
-                <p>berry firmness name:</p>
-                <span>{this.state.fetchResult?.firmness.name}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry firmness url: </p>
-                <span>{this.state.fetchResult?.firmness.url}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry natural_gift_power:</p>
-                <span>{this.state.fetchResult?.natural_gift_power}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry size:</p>
-                <span data-testid="berry-size">{this.state.fetchResult?.size}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry id:</p>
-                <span>{this.state.fetchResult?.id}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry name:</p>
-                <span >{this.state.fetchResult?.name}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry natural_gift_power:</p>
-                <span>{this.state.fetchResult?.natural_gift_power}</span>
-              </li>
-              <li className='listApi__el-info'>
-                <p>berry smoothness:</p>
-                <span>{this.state.fetchResult?.smoothness}</span>
-              </li>
-            </ul>
-            <ErrorBtn onClick={this.ErrorClick}></ErrorBtn>
-          </div>
-        </section>
-      );
-    }
-
+  if(contentState.loading) {
     return (
       <section className='contentBlock' data-testid="content-block">
         <div className='contentBlock__top'>
-          <HandleForm onClick={this.onClick}></HandleForm>
+          <HandleForm ></HandleForm>
+        </div>
+        <div className='contentBlock__middle'>
+          <div className='loadingIcon'>
+            <img data-testid="loader-icon" src={loaderIcon} alt='loading icon'></img>
+          </div>
+        </div>
+      </section>
+    )
+  }
+
+  if(contentState.searchResult) {
+    return (
+      <section className='contentBlock' data-testid="content-block">
+        <div className='contentBlock__top'>
+          <HandleForm onClick={onClick}></HandleForm>
         </div>
         <div className='contentBlock__middle'>
           <ul className='listApi'>
-            {data && data.map((berry) => (
-              <li key={berry.name} className='listApi__el'>
-                <h2 >{berry.name} - </h2>
-                <div className='listApi__el-info'>
-                  <p >url:</p>
-                  <a href={berry.url}>{berry.url}</a>
-                </div>
-              </li>
-            ))}
+            <li className='listApi__el-info'>
+              <p>berry firmness name:</p>
+              <span>{contentState.fetchResult?.firmness.name}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry firmness url: </p>
+              <span>{contentState.fetchResult?.firmness.url}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry natural_gift_power:</p>
+              <span>{contentState.fetchResult?.natural_gift_power}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry size:</p>
+              <span data-testid="berry-size">{contentState.fetchResult?.size}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry id:</p>
+              <span>{contentState.fetchResult?.id}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry name:</p>
+              <span >{contentState.fetchResult?.name}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry natural_gift_power:</p>
+              <span>{contentState.fetchResult?.natural_gift_power}</span>
+            </li>
+            <li className='listApi__el-info'>
+              <p>berry smoothness:</p>
+              <span>{contentState.fetchResult?.smoothness}</span>
+            </li>
           </ul>
-          <ErrorBtn onClick={this.ErrorClick}></ErrorBtn>
+          <ErrorBtn onClick={ErrorClick}></ErrorBtn>
         </div>
       </section>
     );
   }
-}
+
+  return (
+    <section className='contentBlock' data-testid="content-block">
+      <div className='contentBlock__top'>
+        <HandleForm onClick={onClick}></HandleForm>
+      </div>
+      <div className='contentBlock__middle'>
+        <ul className='listApi'>
+          {contentState.data && contentState.data.map((berry) => (
+            <li key={berry.name} className='listApi__el'>
+              <h2 >{berry.name} - </h2>
+              <div className='listApi__el-info'>
+                <p >url:</p>
+                <a href={berry.url}>{berry.url}</a>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <ErrorBtn onClick={ErrorClick}></ErrorBtn>
+      </div>
+    </section>
+  );
+};
 
 export default ContentBlock;
