fd476849a2ce5337b73409a3382edafb9f721182
Vlad-Vasinev <vda5petr@rambler.ru>
2025-07-27 17:48:53 +0300

    fixed maintsx

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -1,20 +1,22 @@
 
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import App from './App'
 import About from './components/about/about'
 import ErrorPage from './components/404/404'
-import { BrowserRouter, Routes, Route } from 'react-router'
 
 import ErrorBoundary from './components/errorBoundary/errorBoundary'
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <ErrorBoundary >
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
-  </ErrorBoundary>
+  
 )
