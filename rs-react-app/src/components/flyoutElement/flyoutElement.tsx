00bd0cdbdaa06747b5f26df2fe5b9f8d51650425
Vlad-Vasinev <vda5petr@rambler.ru>
2025-08-02 14:01:19 +0300

    feature: csv download

--- rs-react-app/src/components/flyoutElement/flyoutElement.tsx
+++ rs-react-app/src/components/flyoutElement/flyoutElement.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import type { AppDispatch, RootState } from '../../store';
 
 import { deleteAll } from '../../store/elementsSlice';
+import { convertToCSV } from '../saveCsv/saveCsv';
 
 const FlyoutElement = () => {
 
+  const downloadLink = React.useRef<HTMLAnchorElement | null>(null)
+
   const dispatch = useDispatch<AppDispatch>()
   const selectedElements = useSelector((state: RootState) => state.items.elements)
+  const selectedData = useSelector((state:RootState) => state.items.data)
 
   function deleteAllFromState () {
     dispatch(deleteAll())
   }
 
+  function downloadData() {
+    console.log(selectedData)
+    let csvString = convertToCSV(selectedData)
+    console.log(csvString)
+    if(csvString) {
+      downloadCSV(csvString)
+    }
+  }
+
+  function downloadCSV(csvString: string) {
+    console.log('csv string is' + ' ' + csvString)
+    const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+
+    if(downloadLink.current) {
+      downloadLink.current.href = url
+    }
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+    }, 1000);
+
+  }
+
   return (
     <div className={selectedElements.length > 0 ? 'flyoutElement _active' : 'flyoutElement'}>
-      <button className='flyoutElement-download'>download all</button>
-      <button className='flyoutElement-unselect' onClick={deleteAllFromState}>unselect all</button>
       <p>you have choosed {selectedElements.length} items</p>
+      <div className='flyoutElement__wrapper'>
+        <a onClick={downloadData} download={`${selectedElements.length}_items`} ref={downloadLink} href="#" className='flyoutElement-download'>download all</a>
+        <button className='flyoutElement-unselect' onClick={deleteAllFromState}>unselect all</button>
+      </div>
     </div>
   );
 };
---
+++ rs-react-app/src/components/saveCsv/saveCsv.ts
@@ -0,0 +1,49 @@
+
+import type { BerryDate } from "../../types/types";
+
+export function convertToCSV(dataArray: BerryDate[] | null) {
+  if (dataArray) {
+    const headers = [
+      'id',
+      'name',
+      'firmness_name',
+      'growth_time',
+      'max_harvest',
+      'natural_gift_power',
+      'natural_gift_type_name',
+      'size',
+      'smoothness',
+      'soil_dryness',
+      'item_name',
+      'flavors'
+    ];
+    const csvRows = [];
+    csvRows.push(headers.join(','));
+
+    dataArray.forEach(item => {
+      const row = [
+        item.id,
+        item.name || '',
+        item.firmness?.name || '',
+        item.growth_time,
+        item.max_harvest,
+        item.natural_gift_power,
+        item.natural_gift_type?.name || '',
+        item.size,
+        item.smoothness,
+        item.soil_dryness,
+        item.item?.name || '',
+        JSON.stringify(item.flavors) || ''
+      ];
+
+      const escapedRow = row.map(value => {
+        return value;
+      });
+
+      csvRows.push(escapedRow.join(','));
+    });
+
+    return csvRows.join('\n');
+  }
+  return '';
+}
